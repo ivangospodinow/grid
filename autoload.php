77dbd601daeb2b071ebcceb21f82fe95784fec7b
Gospodinow <Gospodinow@DESKTOP-MUJQFQ6>
2017-10-22 18:28:03 +0300

    autoload

--- autoload.php
+++ autoload.php
@@ -1,6 +1,4 @@
 <?php
-//require_once 'vendor/autoload.php';
-
 spl_autoload_register(function ($name) {
     if (substr($name, 0, 4) === 'Grid') {
         $file = __DIR__ . '/src/' .str_replace('\\', '/', $name) . '.php';
